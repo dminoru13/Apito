07278d0e516967765929ce66e89c37246230b98d
notridani <musicodorpg@gmail.com>
2026-04-13 14:04:32 -0300

    Initial commit

---
+++ src/main/java/net/notridani/apito/ApitoClient.java
@@ -0,0 +1,10 @@
+package net.notridani.apito;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ApitoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/notridani/apito/ApitoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.notridani.apito;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ApitoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
