e6f1a8dbe8f42bfcbc43b1947a3e9b39e2c20141
notridani <musicodorpg@gmail.com>
2026-04-13 16:25:18 -0300

    criei um item yey

---
+++ src/main/java/net/notridani/apito/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.notridani.apito.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.notridani.apito.Apito;
+
+public class ModItems {
+    public static final Item FOSSILIZED_SCRAP = registerItem("fossilized_scrap", new Item(new Item.Settings()));
+    public static final Item ROSELITA = registerItem("roselita", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Apito.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems() {
+
+        Apito.LOGGER.info("Registering Mod Items for " + Apito.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(FOSSILIZED_SCRAP);
+            entries.add(ROSELITA);
+        });
+    }
+}
